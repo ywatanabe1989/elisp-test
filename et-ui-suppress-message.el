2a39fc8344df7c39bde4c0089ad3254e432d365c
Yusuke Watanabe <ywatanabe@alumni.u-tokyo.ac.jp>
2025-05-09 18:21:23 +1000

    Remove suppress message from the file

--- et-ui-suppress-message.el
+++ et-ui-suppress-message.el
@@ -1,6 +1,6 @@
 ;;; -*- coding: utf-8; lexical-binding: t -*-
 ;;; Author: ywatanabe
-;;; Timestamp: <2025-05-09 14:38:10>
+;;; Timestamp: <2025-05-09 17:17:46>
 ;;; File: /home/ywatanabe/.emacs.d/lisp/elisp-test/et-ui-suppress-message.el
 
 ;;; Copyright (C) 2025 Yusuke Watanabe (ywatanabe@alumni.u-tokyo.ac.jp)
@@ -8,19 +8,19 @@
 
 ;; Suppress ERT messages during test execution
 
-(defun elisp-test--suppress-messages (orig-fun &rest args)
-  "Advice to suppress messages from ERT during test execution."
-  (let ((inhibit-message t)
-        (message-log-max nil))
-    (apply orig-fun args)))
+;; (defun elisp-test--suppress-messages (orig-fun &rest args)
+;;   "Advice to suppress messages from ERT during test execution."
+;;   (let ((inhibit-message t)
+;;         (message-log-max nil))
+;;     (apply orig-fun args)))
 
-;; Add advice to ert functions that output messages
-(advice-add 'ert-run-tests-batch-and-exit :around
-            #'elisp-test--suppress-messages)
-(advice-add 'ert-run-test-interactively :around
-            #'elisp-test--suppress-messages)
-(advice-add 'ert--pp-with-indentation-and-newline :around
-            #'elisp-test--suppress-messages)
+;; ;; Add advice to ert functions that output messages
+;; (advice-add 'ert-run-tests-batch-and-exit :around
+;;             #'elisp-test--suppress-messages)
+;; (advice-add 'ert-run-test-interactively :around
+;;             #'elisp-test--suppress-messages)
+;; (advice-add 'ert--pp-with-indentation-and-newline :around
+;;             #'elisp-test--suppress-messages)
 
 
 (provide 'et-ui-suppress-message)
