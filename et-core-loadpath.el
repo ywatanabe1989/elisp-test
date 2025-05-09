51f34cbe4f09bf7d8de2582a98c20bb07f225a06
Yusuke Watanabe <ywatanabe@alumni.u-tokyo.ac.jp>
2025-05-09 14:29:16 +1000

    Update timestamp in et-core-loadpath.el

--- et-core-loadpath.el
+++ et-core-loadpath.el
@@ -1,22 +1,10 @@
 ;;; -*- coding: utf-8; lexical-binding: t -*-
 ;;; Author: ywatanabe
-;;; Timestamp: <2025-05-09 13:07:56>
+;;; Timestamp: <2025-05-09 14:28:45>
 ;;; File: /home/ywatanabe/.emacs.d/lisp/elisp-test/et-core-loadpath.el
 
 ;;; Copyright (C) 2025 Yusuke Watanabe (ywatanabe@alumni.u-tokyo.ac.jp)
 
-;;; elisp-test-core-loadpath.el --- Load path handling for elisp-test -*- lexical-binding: t -*-
-
-;; Author: ywatanabe
-;; Timestamp: <2025-05-09 02:12:00>
-
-;; Copyright (C) 2025 Yusuke Watanabe (ywatanabe@alumni.u-tokyo.ac.jp)
-
-;;; Commentary:
-
-;; Handles load paths for the elisp-test framework.
-
-;;; Code:
 
 (require 'et-core-variables)
 
@@ -32,8 +20,6 @@
           (expand-file-name path)))
       (add-to-list 'elisp-test-loadpath full-path)
       (add-to-list 'load-path full-path))))
-
-;;; elisp-test-core-loadpath.el ends here
 
 
 (provide 'et-core-loadpath)
